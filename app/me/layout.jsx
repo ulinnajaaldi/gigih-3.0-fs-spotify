7d0c13908e2d36bf911b60aa80eb2846928f49e8
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-08-08 01:58:19 +0700

    style: adding flex grow for contents layout

--- app/me/layout.jsx
+++ app/me/layout.jsx
@@ -5,9 +5,11 @@ export default function MainLayout({ children }) {
   return (
     <main className="flex items-start justify-start">
       <NavbarUser />
-      <Sidebar />
+      <div className="z-10">
+        <Sidebar />
+      </div>
       <section className="h-screen flex-grow bg-black py-2">
-        <div className="h-full max-h-screen overflow-auto rounded-[8px] bg-gradient-to-b from-neutral-800 to-neutral-900 px-4 py-3">
+        <div className="h-full max-h-screen overflow-auto rounded-[8px]">
           {children}
         </div>
       </section>
